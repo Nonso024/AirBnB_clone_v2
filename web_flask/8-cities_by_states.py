fbd1244bf1ed1a2bcede46fdc57099aea0a94844
Nonso024 <josephani772@gmail.com>
2023-05-21 16:11:58 +0100

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
-"""
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
 from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """This displays an HTML page with a list of all states and realted cities.
-    They are sorted by name.
-    """
-    states = storage.all(State)
-    return render_template("8_cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy session."""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port="5000")
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a html page with states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
